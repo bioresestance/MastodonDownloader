9599de8a707a6c6d0b15fa8c2414f6e25d194d9c
Aaron Bromma <aaron@bromma.dev>
2025-01-27 20:12:44 -0800

    Refactor settings management to support command-line arguments for configuration

--- settings.py
+++ settings.py
@@ -1,18 +1,29 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 from pydantic import Field
 from functools import lru_cache
+import argparse
 
 class Settings(BaseSettings):
     access_code: str = Field("", env="ACCESS_CODE")
     mastodon_url: str = Field("https://mastodon.social", env="MASTODON_URL")
     mastodon_user: str = Field("", env="MASTODON_USER")
 
-
-
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-
-
-
 @lru_cache()
 def get_settings() -> Settings:
-    return Settings()
+    parser = argparse.ArgumentParser(description="Mastodon Downloader Settings")
+    parser.add_argument("--access_code", type=str, help="Access code for Mastodon API")
+    parser.add_argument("--mastodon_url", type=str, help="Mastodon instance URL")
+    parser.add_argument("--mastodon_user", type=str, help="Mastodon username")
+
+    args = parser.parse_args()
+
+    settings = Settings()
+
+    if args.access_code:
+        settings.access_code = args.access_code
+    if args.mastodon_url:
+        settings.mastodon_url = args.mastodon_url
+    if args.mastodon_user:
+        settings.mastodon_user = args.mastodon_user
+
+    return settings
